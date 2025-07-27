f15716541c99ddc38ad0f5257e4f50d6df37ad56
Ariel Rodriguez <francis.rodriguez01@epn.edu.ec>
2025-07-27 17:36:50 -0500

    tercer commit

--- demo/demo.java
+++
@@ -1,9 +0,0 @@
-package demo;
-
-public class demo {
-	public static void main(String[] args) {
-		System.out.println("Hola mundo >:3");
-		System.out.println("como tas");
-		System.out.println("yo muy bien uwu");
-	}
-}
--- demo/demo3.java
+++
@@ -1,7 +0,0 @@
-package demo;
-
-public class demo3 {
-	public static void main(String[] args) {
-		System.out.println("Hola soy nuevo :3");
-	}
-}
